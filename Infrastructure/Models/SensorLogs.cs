607f7aab4ab0d381216a7b9dc25bd986b5e701d7
ShravanRamjathan <shravanramjathan@gmail.com>
2025-09-15 10:21:22 +0200

    Added a new model for monbodb to log sensor readings

--- Infrastructure/Models/SensorLogs.cs
+++ Infrastructure/Models/SensorLogs.cs
@@ -3,21 +3,27 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Smart_Roots_Server.Infrastructure.Models {
     public class SensorLogs {
-      
-            [BsonId]
-            [BsonRepresentation(BsonType.ObjectId)]
-            public string? Id { get; set; }
 
-            [BsonElement("Name")]
-            public int Temperature { get; set; };
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string MacAddress { get; set; } // this is where the data will be coming from
 
-            public int Ec { get; set; }
+        [BsonElement("Temperature")]
+        public int Temperature { get; set; }
 
-            public int FlowRate { get; set; }
+        [BsonElement("Ec")]
+        public int Ec { get; set; }
+        [BsonElement("FlowRate")]
+        public int FlowRate { get; set; }
+        [BsonElement("PH")]
+        public int PH { get; set; }
+        [BsonElement("Light")]
+        public int Light { get; set; }
+        [BsonElement("Humidity")]
+        public int Humidity { get; set; }
 
-            public int PH { get; set; }
-        public int Light {  get; set; }
-
+        public DateTime Created_At { get; set; }
         
     }
 }
