a8717d805cb24187513f98f13b63f8792c8c8a99
ShravanRamjathan <shravanramjathan@gmail.com>
2025-10-01 07:39:42 +0200

    Updating sensor model to reflect data thats coming in

--- Infrastructure/Dtos/SensorReadingDto.cs
+++ Infrastructure/Dtos/SensorReadingDto.cs
@@ -5,14 +5,12 @@
     {
         public string? MacAddress { get; set; }
         public double? Temperature { get; set; }
-        public double? Ec { get; set; }
+        public double? EC { get; set; }
         public double? FlowRate { get; set; }
         public double? PH { get; set; }
         public double? Light { get; set; }
         public double? Humidity { get; set; }
 
-        // If device provides timestamps, we’ll prefer them
-        public System.DateTime? ts { get; set; }
-        public System.DateTime? createdAt { get; set; }
+    
     }
 }
